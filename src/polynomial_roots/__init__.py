4aeb8e901de26cfdd45a57b2276c50f15a85c677
Dhya El Bahri <disciple0fmarx@tuta.io>
2026-01-29 00:45:28 +0100

    chore: refine project structure

---
+++ src/polynomial_roots/__init__.py
@@ -0,0 +1,12 @@
+from ._version import __version__
+from .quadratic import QuadraticRoots
+from .utils import format_polynomial, format_roots
+
+
+__all__ = [
+    "QuadraticRoots",
+    "format_polynomial",
+    "format_roots",
+    "__version__",
+]
+
---
+++ src/polynomial_roots/__main__.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    from . import __version__
+    print(f"polynomial-roots v{__version__}")
+    print("Try: from polynomial_roots import QuadraticRoots")
+
